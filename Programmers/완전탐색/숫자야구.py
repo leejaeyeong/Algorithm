c3dac6e8c7f3a1607a706b8a066005dd16ac0e26
이재영 <dldustn14@gmail.com>
2020-02-08 23:59:29 +0900

    2020-08-08 숫자야구.py

--- Programmers/완전탐색/숫자야구.py
+++ Programmers/완전탐색/숫자야구.py
@@ -1,5 +1,27 @@
+""" 각자 서로 다른 1~9까지 3자리 임의의 숫자를 정한 뒤 서로에게 3자리의 숫자를 불러서 결과를 확인합니다. 
+그리고 그 결과를 토대로 상대가 정한 숫자를 예상한 뒤 맞힙니다.
+
+* 숫자는 맞지만, 위치가 틀렸을 때는 볼
+* 숫자와 위치가 모두 맞을 때는 스트라이크
+* 숫자와 위치가 모두 틀렸을 때는 아웃
+
+예를 들어, 아래의 경우가 있으면
+A : 123
+B : 1스트라이크 1볼.
+A : 356
+B : 1스트라이크 0볼.
+A : 327
+B : 2스트라이크 0볼.
+A : 489
+B : 0스트라이크 1볼.
+이때 가능한 답은 324와 328 두 가지입니다.
+
+질문한 세 자리의 수, 스트라이크의 수, 볼의 수를 담은 2차원 배열 baseball이 매개변수로 주어질 때, 
+가능한 답의 개수를 return 하도록 solution 함수를 작성해주세요.
+ """
 def solution(baseball):
     posible = []
+    # 유효한 범위는 중복 x, 1~9
     for i in range(123,987+1) :
         if str(i)[0] == str(i)[1] or str(i)[0] == str(i)[2] or str(i)[1] == str(i)[2] or str(i)[0] == '0' or str(i)[1] == '0' or str(i)[2] == '0' :
             continue
@@ -7,6 +29,8 @@ def solution(baseball):
             posible.append(str(i))
     
     cnt = 0 
+    # 가능한 모든 경우를 정답으로 가정하고
+    # 문제에서 주어지는 num으로 strike, ball이 일치하는지 확인 
     for i in posible :
         flag = True 
         for j in baseball :
