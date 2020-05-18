d1f63dced21c73201f8291e9e8654b333966c48c
이재영 <dldustn14@gmail.com>
2020-05-19 00:55:57 +0900

    2020-05-19 방문길이 수정

--- Programmers/Summer,Winter Coding/방문길이.py
+++ Programmers/Summer,Winter Coding/방문길이.py
@@ -4,65 +4,57 @@ def solution(dirs):
 
     def movePostion(k) :
         position = str(currentPosition[0])+','+str(currentPosition[1])
+        upPosition = str(currentPosition[0])+','+str(currentPosition[1]+1)
+        downPosition = str(currentPosition[0])+','+str(currentPosition[1]-1)
+        leftPosition = str(currentPosition[0]-1)+','+str(currentPosition[1])
+        rightPosition = str(currentPosition[0]+1)+','+str(currentPosition[1])
+
         if k == 'U' and abs(currentPosition[1] + 1) <= 5 :
-            if str(currentPosition[0])+','+str(currentPosition[1]+1) in positions.keys() :
-                if 'D' in positions[str(currentPosition[0])+','+str(currentPosition[1]+1)] :
-                    currentPosition[1] = currentPosition[1] + 1
-                    return False
-            if position in positions.keys() :
+            currentPosition[1] = currentPosition[1] + 1
+            if upPosition in positions.keys() and 'D' in positions[upPosition] :
+                return False
+            elif position in positions.keys() :
                 if 'U' in positions[position] :
-                    currentPosition[1] = currentPosition[1] + 1
                     return False
-                else :
-                    positions[position] += 'U'
+                positions[position] += 'U'
             else :
                 positions[position] = 'U'
-            currentPosition[1] = currentPosition[1] + 1
             return True
+
         elif k == 'D' and abs(currentPosition[1] - 1) <= 5 :
-            if str(currentPosition[0])+','+str(currentPosition[1]-1) in positions.keys() :
-                if 'U' in positions[str(currentPosition[0])+','+str(currentPosition[1]-1)] :
-                    currentPosition[1] = currentPosition[1] - 1
-                    return False
+            currentPosition[1] = currentPosition[1] - 1
+            if downPosition in positions.keys() and 'U' in positions[downPosition] :
+                return False
             if position in positions.keys() :
                 if 'D' in positions[position] :
-                    currentPosition[1] = currentPosition[1] - 1
                     return False
-                else :
-                    positions[position] += 'D'
+                positions[position] += 'D'
             else :
                 positions[position] = 'D'
-            currentPosition[1] = currentPosition[1] - 1
             return True
+
         elif k == 'L' and abs(currentPosition[0] - 1) <= 5 :
-            if str(currentPosition[0]-1)+','+str(currentPosition[1]) in positions.keys() :
-                if 'R' in positions[str(currentPosition[0]-1)+','+str(currentPosition[1])] :
-                    currentPosition[0] = currentPosition[0] - 1 
-                    return False
+            currentPosition[0] = currentPosition[0] - 1 
+            if leftPosition in positions.keys() and 'R' in positions[leftPosition] :
+                return False
             if position in positions.keys() :
                 if 'L' in positions[position] :
-                    currentPosition[0] = currentPosition[0] - 1 
                     return False
-                else :
-                    positions[position] += 'L'
+                positions[position] += 'L'
             else :
                 positions[position] = 'L'
-            currentPosition[0] = currentPosition[0] - 1 
             return True
+
         elif k == 'R' and abs(currentPosition[0] + 1) <= 5 :
-            if str(currentPosition[0]+1)+','+str(currentPosition[1]) in positions.keys() :
-                if 'L' in positions[str(currentPosition[0]+1)+','+str(currentPosition[1])] :
-                    currentPosition[0] = currentPosition[0] + 1 
-                    return False
+            currentPosition[0] = currentPosition[0] + 1 
+            if rightPosition in positions.keys() and 'L' in positions[rightPosition] :
+                return False
             if position in positions.keys() :
                 if 'R' in positions[position] :
-                    currentPosition[0] = currentPosition[0] + 1 
                     return False
-                else :
-                    positions[position] += 'R'
+                positions[position] += 'R'
             else :
                 positions[position] = 'R'
-            currentPosition[0] = currentPosition[0] + 1 
             return True
         return False
 
