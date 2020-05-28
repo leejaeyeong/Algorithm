5f3db1cbd45a67493bf22cbdb5456933a3611921
이재영 <dldustn14@gmail.com>
2020-05-29 04:15:15 +0900

    2020-05-29 달팽이배열

---
+++ Baekjoon/달팽이배열.py
@@ -0,0 +1,28 @@
+rows, cols = 11, 11
+arr = [[0 for col in range(cols)] for row in range(rows)] # 5x5 배열 생성
+
+startRow, endRow = 0, rows
+startCol, endCol = 0, cols
+i, j = 0,0
+
+for num in range(1,rows*cols+1) :
+    arr[i][j] = num 
+    if i == startRow and j + 1 != endCol and arr[i][j+1] == 0 :
+        j += 1
+        if j + 1 == endCol :
+            startRow += 1
+    elif j == endCol-1 and i + 1 != endRow and arr[i+1][j] == 0 :
+        i += 1
+        if i + 1 == endRow :
+            endCol -= 1
+    elif i == endRow-1 and  j - 1 >= startCol and arr[i][j-1] == 0 :
+        j -= 1
+        if j - 1 < startCol :
+            endRow -= 1
+    elif j == startCol and i - 1 >= startRow and arr[i-1][j] == 0 :
+        i -= 1
+        if i - 1 <  startRow :
+            startCol += 1
+
+for i in arr :
+    print(i)
