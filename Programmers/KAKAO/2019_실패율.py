e3276918646c811c35050936b9edd05a85b920d7
이재영 <dldustn14@gmail.com>
2020-07-03 17:20:21 +0900

    2020-07-03 실패율 다시풀기

--- Programmers/KAKAO/2019_실패율.py
+++ Programmers/KAKAO/2019_실패율.py
@@ -1,3 +1,4 @@
+# 풀이 1
 def solution(N, stages):
     fail_rates = []
     for i in range(1,N+1) :
@@ -15,6 +16,21 @@ def solution(N, stages):
       answer.append(idx+1)
       fail_rates[idx] = -1
     return answer
+
+# 풀이 2
+def solution2(N, stages):
+    fail_rates = {}
+    for i in range(1,N+1) :
+      unsucceeded, arived = 0, 0
+      for j in stages :
+        if j >= i : # 스테이지에 도달
+          arived += 1
+          if j == i : # 스테이지 클리어 못함
+            unsucceeded += 1
+      fail_rates[i] = 0 if arived == 0 else  unsucceeded/arived
+    
+    return sorted(fail_rates,key=lambda x: fail_rates[x], reverse=True)
+    # 또는 return sorted(fail_rates,key=fail_rates.get, reverse=True)
 
 
 print(solution(5,	[2, 1, 2, 6, 2, 4, 3, 3]))	#[3,4,2,1,5]
